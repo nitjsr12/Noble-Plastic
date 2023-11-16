126525f77da36cc1fa4295bfa88558b353092a1c
Ravi kumar <rk5892962@gmail.com>
2023-11-16 12:33:00 +0530

    Initial commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d64bf1c56e3ac016"; }
+/******/ 		__webpack_require__.h = function() { return "16165a38add4c283"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
